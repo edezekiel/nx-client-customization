017c70084f52e4f3a3cfb5e44fe442d999f634bb
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2024-05-15 10:35:19 -0500

    feat: Add buttons showing color theme

--- apps/nx-client-customization/src/app/nx-welcome.component.ts
+++ apps/nx-client-customization/src/app/nx-welcome.component.ts
@@ -5,7 +5,21 @@ import { CommonModule } from '@angular/common';
   selector: 'app-nx-welcome',
   standalone: true,
   imports: [CommonModule],
-  template: `<div>Hello World</div>`,
+  template: `<div class="container">
+    <h1>Hello World</h1>
+    <div>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
+    </div>
+  </div>`,
   styles: [],
 })
 export class NxWelcomeComponent {}
