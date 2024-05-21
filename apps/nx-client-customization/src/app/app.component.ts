d992cfc9bd434636908ac4608764794e40d1a898
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2024-05-21 11:12:10 -0500

    feat: Create scoped Bootstrap v5 styles

--- apps/nx-client-customization/src/app/app.component.ts
+++ apps/nx-client-customization/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BootstrapV3Component } from './bootstrap-v3.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, BootstrapV3Component],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ apps/nx-client-customization/src/app/bootstrap-v3.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-bootstrap-v3',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<div class="container">
+    <h1>Hello World</h1>
+    <div>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
+    </div>
+  </div>`,
+  styles: [],
+})
+export class BootstrapV3Component {}
